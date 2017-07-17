7c03ffdf8e9826ccb8ee5a94a010917c303b76be
Keshal Desai <novalearngood@gmail.com>
2017-07-17 12:10:29 -0400

    user info styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import User from "./User";
 
 class Sidebar extends Component {
   render() {
-    return <div className="sidebar" />;
+    return (
+      <div className="sidebar">
+        <User />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Sidebar/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function User() {
+  return (
+    <div className="user">
+      <div className="user-icon">
+        <img src="http://lorempixel.com/35/35/cats/" alt="user icon" />
+      </div>
+      <div className="user-name">John Smith</div>
+    </div>
+  );
+}
+
+export default User;
