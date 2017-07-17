85419b926d2d1a04e1bd130040e9bd7253326858
Keshal Desai <novalearngood@gmail.com>
2017-07-17 11:28:13 -0400

    react router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard";
 
 class App extends Component {
   render() {
-    return <div className="app">App</div>;
+    return (
+      <Router>
+        <div className="app">
+          App
+          <Route path="/dashboard" component={Dashboard} />
+        </div>
+      </Router>
+    );
   }
 }
 
