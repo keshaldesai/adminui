451ed3fac0eedc9e0acfb91d25667da24cb52f37
Keshal Desai <novalearngood@gmail.com>
2017-07-17 11:38:20 -0400

    basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
+import Sidebar from "./Sidebar/Sidebar";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="app">
-          App
+          <Sidebar />
           <Route path="/dashboard" component={Dashboard} />
         </div>
       </Router>
