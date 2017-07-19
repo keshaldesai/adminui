15566ae2e47a27eacbdc868321576297f9bc2773
Keshal Desai <novalearngood@gmail.com>
2017-07-19 13:56:59 -0400

    styled tab children

--- src/components/Sidebar/Tabs/TabItem.js
+++ src/components/Sidebar/Tabs/TabItem.js
@@ -4,11 +4,15 @@ function TabItem({ icon, title, active, children }) {
   const activeClass = active ? " tab-item-active" : "";
   return (
     <div className={"tab-item" + activeClass}>
-      <i className={`fa fa-${icon}`} aria-hidden="true" />
-      <div className="tab-item-title">
-        {title}
+      <div className="tab-item-header">
+        <i className={`fa fa-${icon}`} aria-hidden="true" />
+        <div className="tab-item-title">
+          {title}
+        </div>
       </div>
-      {children}
+      <div className="tab-item-children">
+        {children}
+      </div>
     </div>
   );
 }
