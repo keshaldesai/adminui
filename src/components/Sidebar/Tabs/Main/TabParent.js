b0eab364ae36ecd3899b73eb22a7536eb8f23648
Keshal Desai <novalearngood@gmail.com>
2017-07-20 14:29:36 -0400

    caret on right

--- src/components/Sidebar/Tabs/Main/TabParent.js
+++ src/components/Sidebar/Tabs/Main/TabParent.js
@@ -1,19 +1,43 @@
 import React, { Component } from "react";
 
 class TabParent extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
   render() {
     const { children, title, icon, active } = this.props;
+    const { show } = this.state;
     const activeClass = active ? " tab-item-active" : "";
+    const caretStyle = show
+      ? { transform: "rotate(-90deg)" }
+      : { transform: "none" };
+    const childStyle = show
+      ? { height: "100%", display: "block" }
+      : { height: "0", display: "none" };
+    console.log(caretStyle, childStyle);
     return (
       <div className={"tab-item" + activeClass}>
-        <div className="tab-item-header">
+        <div className="tab-item-header" onClick={this.handleClick}>
           <i className={`fa fa-${icon}`} aria-hidden="true" />
           <div className="tab-item-title">
             {title}
           </div>
-          <i className="fa fa-caret-left" aria-hidden="true" />
+          <div className="tab-item-open" style={caretStyle}>
+            <i className="fa fa-caret-left fa-lg" aria-hidden="true" />
+          </div>
         </div>
-        <div className="tab-item-children">
+        <div className="tab-item-children" style={childStyle}>
           {children}
         </div>
       </div>
