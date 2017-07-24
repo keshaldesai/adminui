67f9093a55c5ff34cdcc3ecb9d06fc0ab66cf5f7
Keshal Desai <novalearngood@gmail.com>
2017-07-24 10:27:37 -0400

    finished social

--- src/components/Sidebar/Tabs/Social/SocialGroup.js
+++ src/components/Sidebar/Tabs/Social/SocialGroup.js
@@ -5,15 +5,22 @@ const SocialGroup = ({ type, users }) => {
     <div className={`social-${type} social-group`}>
       {users.map(user => {
         const { name, picture, login } = user;
+        const fullName = `${capitalize(name.first)} ${capitalize(name.last)}`;
         return (
           <div className="social-user" key={login.username}>
             <img src={picture.thumbnail} alt="thumbnail" />
-            {`${name.first} ${name.last}`}
+            <div className="social-name">
+              {fullName}
+            </div>
           </div>
         );
       })}
     </div>
   );
 };
+
+function capitalize(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
 
 export default SocialGroup;
