dbea1358ec03189ccbedb74668d00c6ad358353a
Keshal Desai <novalearngood@gmail.com>
2017-07-24 13:08:06 -0400

    refactor to stop additional api calls

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,14 +6,28 @@ import SocialTab from "./Tabs/Social/SocialTab";
 import StatisticsTab from "./Tabs/StatisticsTab";
 import HistoryTab from "./Tabs/HistoryTab";
 import AlertTab from "./Tabs/AlertTab";
+import axios from "axios";
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      activeTab: 0
+      activeTab: 0,
+      users: null
     };
+  }
+
+  componentWillMount() {
+    axios
+      .get(
+        "https://randomuser.me/api/?inc=name,picture,login&results=14&nat=us"
+      )
+      .then(data =>
+        this.setState({
+          users: data.data.results
+        })
+      );
   }
 
   handleClick(index) {
@@ -21,13 +35,13 @@ class Sidebar extends Component {
   }
 
   renderTab() {
-    const { activeTab } = this.state;
+    const { activeTab, users } = this.state;
     const { match } = this.props;
     switch (activeTab) {
       case 0:
         return <MainTab activePage={match.url} />;
       case 1:
-        return <SocialTab />;
+        return <SocialTab users={users} />;
       case 2:
         return <StatisticsTab />;
       case 3:
--- src/components/Sidebar/Tabs/Social/SocialTab.js
+++ src/components/Sidebar/Tabs/Social/SocialTab.js
@@ -1,49 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import SocialGroup from "./SocialGroup";
-import axios from "axios";
 
-class SocialTab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: null
-    };
+const SocialTab = ({ users }) => {
+  if (!users || users.length !== 14) {
+    return <div className="tab tab-social" />;
   }
-
-  componentWillMount() {
-    axios
-      .get(
-        "https://randomuser.me/api/?inc=name,picture,login&results=14&nat=us"
-      )
-      .then(data =>
-        this.setState({
-          users: data.data.results
-        })
-      );
-  }
-
-  render() {
-    const { users } = this.state;
-    if (!users || users.length !== 14) {
-      return <div className="tab tab-social" />;
-    }
-    const online = users.slice(0, 4);
-    const idle = users.slice(4, 7);
-    const busy = users.slice(7, 11);
-    const offline = users.slice(11);
-    return (
-      <div className="tab tab-social">
-        <div className="tab-title">ONLINE (4)</div>
-        <SocialGroup type="online" users={online} />
-        <div className="tab-title">IDLE (3)</div>
-        <SocialGroup type="idle" users={idle} />
-        <div className="tab-title">BUSY (4)</div>
-        <SocialGroup type="busy" users={busy} />
-        <div className="tab-title">OFFLINE (3)</div>
-        <SocialGroup type="offline" users={offline} />
-      </div>
-    );
-  }
-}
+  const online = users.slice(0, 4);
+  const idle = users.slice(4, 7);
+  const busy = users.slice(7, 11);
+  const offline = users.slice(11);
+  return (
+    <div className="tab tab-social">
+      <div className="tab-title">ONLINE (4)</div>
+      <SocialGroup type="online" users={online} />
+      <div className="tab-title">IDLE (3)</div>
+      <SocialGroup type="idle" users={idle} />
+      <div className="tab-title">BUSY (4)</div>
+      <SocialGroup type="busy" users={busy} />
+      <div className="tab-title">OFFLINE (3)</div>
+      <SocialGroup type="offline" users={offline} />
+    </div>
+  );
+};
 
 export default SocialTab;
