3c320f4857e2829d524bbcc4d5192c719c688420
Keshal Desai <novalearngood@gmail.com>
2017-07-26 13:41:54 -0400

    finished buttons

--- src/components/Extras/Button.js
+++ src/components/Extras/Button.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Button = ({ children, color }) => {
-  return (
-    <div className="button" style={{ color, borderColor: color }}>
-      {children}
-    </div>
-  );
-};
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hover: false, active: false };
+  }
+
+  render() {
+    const { children, color, handleClick, active } = this.props;
+    const { hover } = this.state;
+    const activeClass = {
+      borderColor: color,
+      backgroundColor: color,
+      color: "white"
+    };
+    const normalClass = { borderColor: color, color: color };
+    const style = hover || active ? activeClass : normalClass;
+    return (
+      <div
+        className="button"
+        style={style}
+        onMouseOver={() => {
+          this.setState({ hover: true });
+        }}
+        onMouseOut={() => {
+          this.setState({ hover: false });
+        }}
+        onClick={handleClick}
+      >
+        {children}
+      </div>
+    );
+  }
+}
 
 export default Button;
--- src/components/Sidebar/Tabs/History/FriendRequest.js
+++ src/components/Sidebar/Tabs/History/FriendRequest.js
@@ -2,13 +2,35 @@ import React, { Component } from "react";
 import Button from "../../../Extras/Button";
 
 class FriendRequest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeButton: 0 };
+  }
+
+  handleClick(activeButton) {
+    this.setState({ activeButton });
+  }
+
   render() {
+    const { activeButton } = this.state;
     return (
       <div className="friendreq">
         Sent you a friend request!
         <div className="friendreq-buttons">
-          <Button color="#3498DB">Accept</Button>
-          <Button color="#E74C3C">Reject</Button>
+          <Button
+            color="#3498DB"
+            handleClick={this.handleClick.bind(this, 1)}
+            active={activeButton === 1}
+          >
+            ACCEPT
+          </Button>
+          <Button
+            color="#E74C3C"
+            handleClick={this.handleClick.bind(this, 2)}
+            active={activeButton === 2}
+          >
+            REJECT
+          </Button>
         </div>
       </div>
     );
