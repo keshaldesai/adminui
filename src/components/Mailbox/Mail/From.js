c865a07577ccc648e07a48508788b80efb3ea67a
Keshal Desai <novalearngood@gmail.com>
2017-08-07 14:55:36 -0400

    from finished

--- src/components/Mailbox/Mail/From.js
+++ src/components/Mailbox/Mail/From.js
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 class From extends Component {
   render() {
-    return <div className="mb-group mail-from">From</div>;
+    return (
+      <div className="mb-block mail-from flex-row">
+        <div className="from-user flex-row">
+          <img
+            src="https://randomuser.me/api/portraits/thumb/men/72.jpg"
+            alt="user"
+          />
+          <div className="from-info">
+            <div className="from-email">
+              From: Rick Olsen - rick.olsen@example.com
+            </div>
+            <div className="from-subject">
+              Subject: Concerning the proposal from our last meeting
+            </div>
+          </div>
+        </div>
+        <div className="from-extra">
+          <div className="from-tag">PROPOSALS</div>
+          <div className="from-date">Jul 15th, 2:40PM</div>
+        </div>
+      </div>
+    );
   }
 }
 
