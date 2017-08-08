24a8a35178db5cb538ed77f510d46e0855d715f7
Keshal Desai <novalearngood@gmail.com>
2017-08-08 14:00:20 -0400

    beginning of inbox

--- src/components/Mailbox/Inbox.js
+++ src/components/Mailbox/Inbox.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 
 class Inbox extends Component {
   render() {
-    return <div className="main inbox">Inbox</div>;
+    return (
+      <div className="main inbox">
+        <div className="mb-block mail-actions">Actions</div>
+        <div className="inbox-container flex-row">
+          <div className="mb-block inbox-mailboxes">Mailboxes</div>
+          <div className="mb-block inbox-items">Items</div>
+        </div>
+      </div>
+    );
   }
 }
 
