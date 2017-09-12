c713a818aba83370ef4e6784253177d5193aa334
Keshal Desai <novalearngood@gmail.com>
2017-09-12 12:36:38 -0400

    form is now controlled component

--- src/components/Mailbox/Compose/Form.js
+++ src/components/Mailbox/Compose/Form.js
@@ -1,17 +1,55 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      to: "",
+      cc: "",
+      bcc: "",
+      subject: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <div className="comp-form">
         <div className="comp-header">To</div>
-        <input type="text" placeholder="Example: john@domain.com" />
+        <input
+          type="text"
+          placeholder="Example: john@domain.com"
+          name="to"
+          value={this.state.to}
+          onChange={this.handleChange}
+        />
         <div className="comp-header">CC</div>
-        <input type="text" />
+        <input
+          type="text"
+          name="cc"
+          value={this.state.cc}
+          onChange={this.handleChange}
+        />
         <div className="comp-header">BCC</div>
-        <input type="text" />
+        <input
+          type="text"
+          name="bcc"
+          value={this.state.bcc}
+          onChange={this.handleChange}
+        />
         <div className="comp-header">Subject</div>
-        <input type="text" placeholder="Subject title here" />
+        <input
+          type="text"
+          placeholder="Subject title here"
+          name="subject"
+          value={this.state.subject}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
