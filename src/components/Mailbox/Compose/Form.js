0c81c4f6c293bab458726c76c00705a8fec407e3
Keshal Desai <novalearngood@gmail.com>
2017-09-12 12:06:11 -0400

    finished inputs

--- src/components/Mailbox/Compose/Form.js
+++ src/components/Mailbox/Compose/Form.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Form extends Component {
   render() {
-    return <div className="comp-form">Form</div>;
+    return (
+      <div className="comp-form">
+        <div className="comp-header">To</div>
+        <input type="text" placeholder="Example: john@domain.com" />
+        <div className="comp-header">CC</div>
+        <input type="text" />
+        <div className="comp-header">BCC</div>
+        <input type="text" />
+        <div className="comp-header">Subject</div>
+        <input type="text" placeholder="Subject title here" />
+      </div>
+    );
   }
 }
 
