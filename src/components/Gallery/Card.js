a715d0de04e1c6654cece926fe235758076a26ca
Keshal Desai <novalearngood@gmail.com>
2017-09-18 15:52:14 -0400

    gallery finished

--- src/components/Gallery/Card.js
+++ src/components/Gallery/Card.js
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
+import FullImg from "./FullImg";
 
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = { active: false };
+    this.state = { active: false, showFull: false };
     this.handleClick = this.handleClick.bind(this);
+    this.handleMagnify = this.handleMagnify.bind(this);
   }
   handleClick() {
     this.setState({ active: !this.state.active });
   }
+  handleMagnify() {
+    this.setState({ showFull: !this.state.showFull });
+  }
   render() {
     const { title, date, likes, url } = this.props.info;
-    const { active } = this.state;
+    const { active, showFull } = this.state;
     const activeStyle = active
       ? { backgroundColor: "#ff0000", borderColor: "#ff0000", color: "#ffffff" }
       : {};
     const newLikes = active ? likes + 1 : likes;
     return (
       <div className="card">
-        <img alt="cardimage" src={url} />
+        <div className="card-img-cont" onClick={this.handleMagnify}>
+          <img alt="cardimage" src={url + "?w=200&h=150&fit=crop"} />
+          <div className="card-img-over" />
+          <div className="card-mag">
+            <i className="fa fa-search-plus fa-2x" aria-hidden="true" />
+          </div>
+        </div>
         <div className="card-title">{title}</div>
         <div className="card-date">{date}</div>
         <div
@@ -29,6 +40,11 @@ class Card extends Component {
           <i className="fa fa-heart" aria-hidden="true" />
           {" " + newLikes}
         </div>
+        {showFull ? (
+          <FullImg url={url} handleMagnify={this.handleMagnify} />
+        ) : (
+          ""
+        )}
       </div>
     );
   }
---
+++ src/components/Gallery/FullImg.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class FullImg extends Component {
+  this;
+  render() {
+    return (
+      <div className="full-img">
+        <div className="full-img-cont">
+          <img src={this.props.url} />
+        </div>
+        <div className="full-img-close" onClick={this.props.handleMagnify}>
+          <i className="fa fa-times fa-2x" aria-hidden="true" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FullImg;
