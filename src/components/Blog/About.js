79fde6d4f40ae418a1ea5126cb570f631fbb99bb
Keshal Desai <novalearngood@gmail.com>
2017-09-27 10:40:36 -0400

    posts about finished

--- src/components/Blog/About.js
+++ src/components/Blog/About.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Panel from "../Shared/Panel";
+import UserIcon from "../Shared/UserIcon";
 
 const About = () => {
   return (
     <Panel>
-      <div className="posts-about">About</div>
+      <div className="flex-row">
+        <div className="posts-about-info">
+          Welcome to my page. Please follow me to get updates on what I'm up to!
+        </div>
+        <div className="posts-about-icon">
+          <UserIcon
+            width="45px"
+            picture="https://randomuser.me/api/portraits/thumb/women/24.jpg"
+            color="#FFB300"
+          />
+        </div>
+      </div>
     </Panel>
   );
 };
